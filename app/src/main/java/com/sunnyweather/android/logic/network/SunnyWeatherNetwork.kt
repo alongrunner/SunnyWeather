a1a2757bc69686faba53b4d72012998d536743e6
1075336878 <1075336878@qq.com>
2021-04-22 21:56:13 +0800

    4/22

--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -12,6 +12,11 @@ import kotlin.coroutines.suspendCoroutine
 object SunnyWeatherNetwork {
     private val placeService = ServiceCreator.create<PlaceService>()
     suspend fun searchPlaces(query: String) = placeService.searchPlace(query).await()
+    private val weatherService = ServiceCreator.create<WeatherService>()
+    suspend fun getDailyWeather(lng: String, lat: String) =
+            weatherService.getDailyWeather(lng, lat).await()
+    suspend fun getRealtimeWeather(lng: String, lat: String) =
+            weatherService.getRealtimeWeather(lng,lat).await()
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
             enqueue(object : Callback<T> {
